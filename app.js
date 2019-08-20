89c4d58fae4097ec229b238acd2aea3a58e2ce42
Juan Nunez <51241293+nunju9a@users.noreply.github.com>
2019-08-19 20:43:47 -0400

    set port, views, static in app.js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+// INITIAL REQUIRE STATEMENTS AND VARIABLES
 const Sequelize = require('sequelize');
 const express = require('express');
 const path = require('path');
 const booksRoute = require('./routes/books');
 const bodyParser = require('body-parser');
+const app = express();
+const portNum = 3000;
+
+// SERVING THE STATIC FILES LOCATED IN THE PUBLIC FOLDER
+app.set("view engine", "pug");                                        // Setting view engine to pug
+app.set("views", path.join(__dirname, "views"));                     // Setting views directory
+app.use("/static", express.static(path.join(__dirname, "public"))); // Serve static files
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
