ac53908278bc53ac457effa584516c5e487b56d5
Juan Nunez <51241293+nunju9a@users.noreply.github.com>
2019-08-21 15:22:12 -0400

    app.js comments

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
+// INITIAL REQUIRE STATEMENTS AND VARIABLES
 const express = require("express");
 const app = express();
 const sqlite = require("sqlite3");
-var path = require("path");
+const path = require("path");
+const routes = require("./routes/index");
+const books = require("./routes/books");
+const sequelize = require("./models").sequelize;
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const sequelize = require("./models").sequelize;
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
 app.use(express.static(path.join(__dirname, "public")));
 
-var routes = require("./routes/index");
-var books = require("./routes/books");
+
 
 app.use("/", routes);
 app.use("/books", books);
 
 app.use((req, res, next) => {
-  const err = new Error("Not Found");
+  const err = new Error("Ooops I Did It Again! Page Not Found");
   err.status = 404;
   next(err);
 });
@@ -33,21 +37,8 @@ app.use((err, req, res, next) => {
   }
 });
 
-// app.use((req, res, next) => {
-//   const err = new Error("Page Not Found");
-//   res.render("page-not-found", { error: err });
-// });
-
-// app.use((err, req, res, next) => {
-//   if (err.status === 404) {
-//     res.render("book-not-found", { error: "This book is not ailable" });
-//   } else {
-//     res.render("error", { error: err.message });
-//   }
-// });
-
 sequelize.sync().then(() => {
-  app.listen(3000, () => console.log("Server is running on port 3000."));
+  app.listen(3000, () => console.log("Successfully running on localhost:3000!"));
 });
 
 module.exports = app;
